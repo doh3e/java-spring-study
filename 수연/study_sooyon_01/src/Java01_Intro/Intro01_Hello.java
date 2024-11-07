912918f32b87e9ed8818b89ca0094447c495af89
SOOYON1122 <pksuyon1122@naver.com>
2024-11-07 09:05:37 +0900

    24.11.07 09:04 수연 - 1주차 과제 진행 중...
    아직 자바랑 친해지기 전...ㅠ

---
+++ 수연/study_sooyon_01/src/Java01_Intro/Intro01_Hello.java
@@ -0,0 +1,12 @@
+package Java01_Intro;
+
+public class Intro01_Hello {
+	// 자동 완성 활용 -> ctrl + spacebar
+	// main 쓰고 ctrl + spacebar
+	public static void main(String[] args) {
+		
+		// sysout 쓰고 ctrl + spacebar
+		System.out.println("Hello, World!!!");
+	}
+	
+}
